837e50a80d648399ad8ce84271363e281cec988c
Cheung Kin Wong <Cheungkin.wong@gmail.com>
2019-05-27 10:53:52 +0200

    add js, on load, mouse over and click

--- assets/js/doge.js
+++ assets/js/doge.js
@@ -1,1 +1,18 @@
-document.getElementById("doge").addEventListener("load", function() {
+document.getElementById("doge").onload = woof();
+function woof() {
+  document.getElementById("doge").innerHTML = "Woof!";
+}
+
+document.getElementById("doge").addEventListener("mouseover", woofwoof);
+function woofwoof() {
+  document.getElementById("doge").innerHTML = "Woof woof!";
+}
+
+document.getElementById("doge").addEventListener("click", picture);
+function picture() {
+  if (document.getElementById("picture").style.display === "block") {
+    document.getElementById("picture").style.display = "none";
+  } else {
+    document.getElementById("picture").style.display = "block";
+  }
+}
